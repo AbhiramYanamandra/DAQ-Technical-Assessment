e9fce2c5e5c52a965d316f0235cf47255af83fa2
Abhiram Yanamandra <ysabhiram@gmail.com>
2023-08-29 15:55:25 +0800

    Fixed errors in part one which meant that currJSON returned undefined values and also updated client.send it actually send currJSON to the client

--- brainstorming.md
+++ brainstorming.md
@@ -7,3 +7,14 @@ Through console.logs and throughly reading battery_emulator.ts we can see that t
 #### problem solution
 A try catch code block can be implemented to catch the syntaxError's. We can then attempt to remove the extra '}' that is the reason for the error then attempt to JSON.parse msg again. 
 If msg being passed in has the problematic '}', it will throw an error, which means that the JSON.parse did not successfully run therefore, attempt to remove the extra '}' then run JSON.parse again.
+
+## Part 2: (Server.ts in streaming folder)
+#### problem diagnosis
+Safe operating window of 20-80 degrees celcius, Add a feature to the backend streaming service
+so that each time the received battery temperature exceeds this range more than 3 times in 5 seconds, 
+the current timestamp is logged to a file named 'incidents.log'.
+#### problem solution
+Have an empty array and in that send all the incoming msg objects within the same 5 seconods time frame
+into that empty array, then iterate through the temperatures of the msg objects in the now populated array 
+and everytime temperature exceeds 80 degrees, add to a count, and when count increases past 3, capture the 
+timestamp of the last object in the array and send it incidents.log file.
--- streaming-service/src/server.ts
+++ streaming-service/src/server.ts
@@ -11,25 +11,38 @@ tcpServer.on('connection', (socket) => {
     console.log('TCP client connected');
     
     socket.on('data', (msg) => {
+        console.log("----------------------");
         console.log(msg.toString());
 
         // HINT: what happens if the JSON in the received message is formatted incorrectly?
         // HINT: see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
-        let currJSON;
+        let currJSON: any;
         try {
             currJSON = JSON.parse(msg.toString());
         } catch(error) {
             if (error instanceof SyntaxError) {
-                let msgString = JSON.stringify(msg);
+                let msgString = msg.toString();
+                console.log("msgString is as such: " + msgString);
                 // regex code to catch the extra '}' and then replace it
                 let msgJSON = msgString.replace(/(\}\s*})/, '}');
                 currJSON = JSON.parse(msgJSON);
             }
         }
 
+        // Part 2: safe operating window of 20-80 degrees celcius, Add a feature to the backend streaming service
+        // so that each time the received battery temperature exceeds this range more than 3 times in 5 seconds, 
+        // the current timestamp is logged to a file named 'incidents.log'.
+        
+        // Declaring an empty array:
+        let msgArray = [];
+        console.log("type of currJSON: "+ typeof(currJSON));
+        console.log("this is currJSON temp: " + currJSON.battery_temperature);
+        console.log("this is currJSON timestamp: " + currJSON.timestamp);
+
+
         websocketServer.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
-              client.send(msg.toString());
+              client.send(JSON.stringify(currJSON));
             }
           });
     });
